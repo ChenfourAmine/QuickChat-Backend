4a275719d4c4981b8916d1e21b9e9c79d6d68a3b
ChenfourAmine <amine.chenfour456@gmail.com>
2024-09-24 02:44:27 +0100

    initial commit

---
+++ src/main/java/org/quickchat/QuickChatBackendApplication.java
@@ -0,0 +1,13 @@
+package org.quickchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickChatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/quickchat/QuickChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quickchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
